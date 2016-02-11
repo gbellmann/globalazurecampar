27e5ea361c75997a4464ac8528829fb5183ab1b2
Matías Quaranta <ealsur@hotmail.com>
2016-02-11 00:04:18 -0300

    moved TimeCircles to minifying JS

--- content/js/layout.js
+++ content/js/layout.js
@@ -1,5 +1,34 @@
 (function(){
     componentHandler.registerUpgradedCallback("MaterialLayout",function(){
-		var layzr = new Layzr();	
+		var layzr = new Layzr();
+        $("#DateCountdown").TimeCircles({
+                        "animation": "smooth",
+                        "bg_width": 0.5,
+                        "fg_width": 0.05333333333333334,
+                        "circle_bg_color": "#60686F",
+                        "time": {
+                            "Days": {
+                                "text": "Días",
+                                "color": "#FFCC66",
+                                "show": true
+                            },
+                            "Hours": {
+                                "text": "Horas",
+                                "color": "#99CCFF",
+                                "show": true
+                            },
+                            "Minutes": {
+                                "text": "Minutos",
+                                "color": "#BBFFBB",
+                                "show": true
+                            },
+                            "Seconds": {
+                                "text": "Segundos",
+                                "color": "#FF9999",
+                                "show": true
+                            }
+                        },
+                        "count_past_zero": false
+                    });	
 	});	
 }());
